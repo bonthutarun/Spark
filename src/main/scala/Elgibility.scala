fe7cb3698f270ff7de8d7f3b9c16c41e76c8761f
bonthu <bonthutarunteja123@gmail.com>
2025-01-16 07:41:34 +0530

    eligible to vote

---
+++ src/main/scala/Elgibility.scala
@@ -0,0 +1,20 @@
+object Elgibility {
+
+  def main (args: Array[String]) : Unit = {
+
+  val age = 20
+
+    if ( age >= 18)
+
+      print("elgible to vote")
+
+    else if (age <= 16)
+
+      print("eligible to drive")
+
+
+    else
+
+      print("nothing")
+  }
+}
