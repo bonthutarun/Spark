b0c21d554cd54194219e794e65dbd5e0a1063a55
bonthu <bonthutarunteja123@gmail.com>
2025-01-16 06:58:01 +0530

    oddcheck with AND

---
+++ src/main/scala/oddcheckAnd.scala
@@ -0,0 +1,20 @@
+object oddcheckAnd {
+
+ def main (args: Array[String]) : Unit = {
+
+   val a= 27
+
+   if (a % 2 != 0 && a % 3 == 0) {
+
+     print("given number is odd and divisble by 3")
+
+   }
+
+   else  {
+     print(" given number is not odd and divisble by 3 ")
+
+   }
+
+
+ }
+}
