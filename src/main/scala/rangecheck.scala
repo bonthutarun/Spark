f8e579862cf816c88a5945c54c515ae942f19da7
bonthu <bonthutarunteja123@gmail.com>
2025-01-16 06:44:44 +0530

    check range

---
+++ src/main/scala/rangecheck.scala
@@ -0,0 +1,14 @@
+object rangecheck {
+
+  def main(args : Array[String]): Unit = {
+
+   val a =  -15
+
+   if (a<=15 || a>10) {
+
+    print("given range is less than -10 and greater than 15")
+   }
+   else
+     print("given range is out of less than -10 and greater than 15")
+  }
+}
