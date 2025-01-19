2cf53ea1db1e911f5896af0b070401e67fe6e767
bonthu <bonthutarunteja123@gmail.com>
2025-01-19 12:07:53 +0530

    if else statement

---
+++ src/main/scala/IFELSE01s.scala
@@ -0,0 +1,24 @@
+
+
+object IFELSE01s {
+
+    def main(args: Array[String]): Unit = {
+
+    val a = 100
+
+      if ( a> 10)
+
+      {
+
+        print("a is postive")
+
+      } else
+
+      {
+        print("a is negative")
+      }
+
+    }
+
+
+}
